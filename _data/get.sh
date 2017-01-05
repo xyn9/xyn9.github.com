3925934ca971753fcde4d25adddbb9e53f5d488f
xyn9 <xyn9.mail+github@gmail.com>
2017-01-06 01:53:07 +0900

    _data/get.sh

---
+++ _data/get.sh
@@ -0,0 +1,3 @@
+#
+curl --get 'https://api.github.com/gists/e7426492b7bb92d260a1' --output 'kenp_gist.json'
+curl --get 'https://gist.githubusercontent.com/xyn9/e7426492b7bb92d260a1/raw/kenp.csv' --output 'kenp.csv'
